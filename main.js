7c6f9987cf6ee9666d4a23a479bd8be5e3659796
Eduardo Sobral Leite <sobraleduardoleite@hotmail.com>
2024-05-16 09:47:06 -0300

    Terceira Alteração

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
 const form = document.getElementById ('form-atividade');
 const campoA = document.getElementById("campoA");
 const campoB = document.getElementById("campoB");
-let validaForm = false;
+let formValido = false;
 
-function validaForm(oForms) {
-    const formComoArray = oForms.split(' ');
-    return formComoArray >= 1;
+function atribuiForm(valorA, valorB) {
+    return parseInt(valorB) > parseInt(valorA); //Aqui valida o valor do campoB e CampoA
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    campoA = parseInt(inputcampoA);
-    campoB = parseInt(inputcampoB);
+    const valorA = campoA.value;
+    const ValorB = camporB.value;
 
-    const mensagemSucesso = `campoA é maior ou igual a CampoB`;
-    const mensagemErro = `campoB é maior ou igual a CampoA`; 
+    const mensagemSucesso = `campoB é maior ou igual a CampoA`;
+    const mensagemErro = `campoA é maior ou igual a CampoB`; 
 
-    formValido = validaForm(oForms.value);
+    formValido = atribuiForm(valorA, valorB); //Aqui é passado os 2 parâmentros a serem validados
     
     if (formValido) {
 
-    campoB >= campoA;
     const containerMensagemSucesso = document.querySelector('.sucess-message');
     containerMensagemSucesso.innerHTML = mensagemSucesso;
     containerMensagemSucesso.style.display = 'block';
@@ -29,30 +27,46 @@ form.addEventListener('submit', function(e) {
     campoA.value = '';
     campoB.value = '';
 
+    document.querySelector('.error-message').style.display = 'none'; //Para deixar oculta a mensagem de erro
+
     } else { 
         campoA.style.border = '1px solid red';
         campoB.style.border = '1px solid red';
-    document.querySelector('.error-message').style.display = 'block';
+    document.querySelector('error-message').innerHTML = mensagemErro; //Aqui vai ficar a mensagem de erro
+    document.querySelector('.error-message').style.display = 'block'; //Exibi a mesma
     }
 
-    })
+    });
 
     campoA.addEventListener('keyup',function(e) {
-        console.log(e.target.value);
-        formValido = validaForm(e.target.value);
+        const valorA = e.target.value;
+        const valorB = campoB.value;
+        formValido = atribuiForm(valorA, valorB);
+
+        if (!formValido) {
+            campoA.style.border = '1px solid red';
+                campoB.style.border = '1px solid red';
+            document.querySelector('.error-message').style.display = 'block'; 
+        } else {
+            campoA.style = '';
+                campoA.style = '';
+                document.querySelector('.error-message').style.display = 'none';
+        }
     });
 
     campoB.addEventListener('keyup',function(e) {
-        console.log(e);
-        formValido = validaForm(e.target.value);
+        const valorA = campoA.value;
+        const valorB = e.target.value;
+        formValido = atribuiForm(valorA, valorB);
 
         if (!formValido) { 
                 campoA.style.border = '1px solid red';
                 campoB.style.border = '1px solid red';
-            document.querySelector('.error-message').style.display = 'block';
+            document.querySelector('error-message').innerHTML = mensagemErro; // Aqui vai ficar a mensagem de erro
+            document.querySelector('.error-message').style.display = 'block'; // Exibi a mesma
             } else {
-                campoA.style = '';
-                campoA.style = '';
+                campoA.style.border = '';
+                campoA.style.border = '';
                 document.querySelector('.error-message').style.display = 'none';
             }
 
